0ef44d25e0257ffe69112c6e776b27cf599f038a
maochengrui <maochengrui@xunlei.com>
2018-06-14 15:00:06 +0800

    selector+tcp

---
+++ app/src/main/java/com/example/andy/andydemo/layout/SelectorActivity.java
@@ -0,0 +1,16 @@
+package com.example.andy.andydemo.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.andy.andydemo.R;
+
+public class SelectorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector);
+    }
+
+}
